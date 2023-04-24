7fcd0812dbe81b5aa6d6f16d45eded05224cf67d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2023-04-24 01:55:29 +0000

    created the printf , the function list, the and edited the main.h . all betty checked.

--- main.h
+++ main.h
@@ -1,7 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+  * struct partof - the struct tag specifier
+  *@ok : the valid char.
+  *@f: the functiosn associated
+ */
 
 
+typedef struct partof
+{
+	char *ok;
+	int (*f)(va_list);
+} quality;
+
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int _putchar(char c);
+int print_percent(va_list args);
+int (*get_func(char x))(va_list args);
 
 #endif /* MAIN_H */
