864d196c32e78842848c4b8e35b8ea41003c1f53
gyasifred <gyasifred@gmail.com>
2023-04-24 10:05:04 +0000

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
+typedef fmt
+{
+	char *type;
+	int (*f)(va_list);
+}fmt_t;
+int _printf(const char *format, ...);
+int _putchar(char c);
 
 
 
